7fc2842ff16a1a508a535ae6ad3d506818f98adc
marlon ansale <marlonvon.ansale@fanduel.com>
2023-04-17 09:45:59 +0100

    initial set up.

---
+++ PokemonPriceTCG/PokemonPriceTCG/PokemonPriceTCGApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonPriceTCGApp.swift
+//  PokemonPriceTCG
+//
+//  Created by Marlon Von Bernales Ansale on 17/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PokemonPriceTCGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
